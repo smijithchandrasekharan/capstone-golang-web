bf04b3c23accd34b955b6e697822a9e1d8ca454f
smijithchandrasekharan <smijith.chandrasekharan@ust.com>
2024-12-14 09:14:22 +0530

    Update loginHandler.go

--- authentication/login/handler/loginHandler.go
+++ authentication/login/handler/loginHandler.go
@@ -3,14 +3,10 @@ package handler
 import (
 	"html/template"
 	"net/http"
+
 )
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
-	//title := r.URL.Path[len("/edit/"):]
-	//p, err := loadPage(title)
-	//if err != nil {
-	//p = &Page{Title: title}
-	//}
 	t, _ := template.ParseFiles("edit.html")
 	t.Execute(w, "he")
 }
